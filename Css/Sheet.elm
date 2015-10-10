4265a90b2f60a8be294ad68163cb50d162232d45
Adam Kowalski <adam.kowalski.work@gmail.com>
2015-10-10 12:50:41 -0700

    Added Sheets

--- Css/Sheet.elm
+++ Css/Sheet.elm
@@ -1,47 +1,50 @@
 module Css.Sheet (createRule, sheetAsString, ruleAsString) where
+
 {-| Create a stylesheet from a list of rules which are a list of Styles
 #Get a single name, value pair as string
 @docs createRule
 @docs sheetAsString
 @docs ruleAsString
 -}
-import Css exposing (Styles)
+
+-- Native Imports
 import String exposing (concat)
-{-| name, value pair
--}
+
+--My Imports
+import Css exposing (Styles)
+
+
+{-| name, value pair -}
 type alias Pair = (String, String)
 type alias Rule =
-  {
-    name : String
+  { name : String
   , styles : Styles
-}
+  }
 
 type alias Sheet = List Rule
 
-{-|
-Create a name, styles pair
---}
+                 
+{-| Create a name, styles pair -}
 createRule : String -> Styles -> Rule
 createRule name styles =
   { name = name
   , styles = styles
   }
 
-{-| Get string representation of a whole sheet
---}
+
+{-| Get string representation of a whole sheet -}
 sheetAsString : Sheet -> String
 sheetAsString sheet =
   concat <| List.map ruleAsString sheet
 
 
-{-| Get string representation of a rule
---}
+{-| Get string representation of a rule -}
 ruleAsString : Rule -> String
 ruleAsString rule =
-  rule.name ++ " {\n" ++ (concat (List.map (\s -> pairAsString s) rule.styles)) ++ "\n }\n"
+  let representation = (concat (List.map (\s -> pairAsString s) rule.styles))
+  in rule.name ++ " {\n" ++ representation ++ "\n }\n"
 
 
-{-| Get string representation of a single pair
---}
+{-| Get string representation of a single pair -}
 pairAsString : Pair -> String
 pairAsString (name, value) = "  " ++ name ++ ": " ++ value ++ ";\n"
